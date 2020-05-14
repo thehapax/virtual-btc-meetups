b31a1a22d20c004a2b9c10fc8671de3259486efd
thehapax <39811582+thehapax@users.noreply.github.com>
2020-05-14 00:56:09 -0700

    add multiple inline options

--- vmeet.py
+++ vmeet.py
@@ -3,6 +3,11 @@ from telethon import TelegramClient, events, sync, utils, functions, Button, cus
 import yaml
 import sys
 import logging
+from logging import handlers
+
+from telethon.tl.types import InputWebDocument
+
+log_path = '/Users/octo/virtual-btc-meetups/logs/logfile'
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -10,9 +15,17 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-# virtual bitcoin meetups
-# https://wiki.fulmo.org/wiki/List_of_Virtual_Bitcoin_and_Lightning_Network_Events
+# Log to a rotating file - why isn't this working?
+level = logging.INFO
+logger.setLevel(level)
+# 5*5MB log files max:
+h = logging.handlers.RotatingFileHandler(log_path, encoding='utf-8', maxBytes=5 * 1024 * 1024, backupCount=5)
+# example of format: 2019-04-05 20:28:45,944  INFO: blah
+h.setFormatter(logging.Formatter("%(asctime)s\t%(levelname)s:%(message)s"))
+h.setLevel(level)
+logger.addHandler(h)
 
+# virtual bitcoin meetups
 fulmo_url = "https://wiki.fulmo.org/wiki/List_of_Virtual_Bitcoin_and_Lightning_Network_Events"
 
 path  = "./"
@@ -34,7 +47,7 @@ client.parse_mode = 'html'
 async def callback(event):
     print(event.data)
     await event.edit('Thank you for clicking {}!'.format(event.data))
-
+    
 
 @client.on(events.NewMessage(incoming=True, outgoing=True))    
 async def new_handler(event):
@@ -51,10 +64,18 @@ async def start_handler(event):
             Button.text('Get New Events', resize=True, single_use=True),
             Button.text('Past Events', resize=True, single_use=True)])
 
+@client.on(events.NewMessage(pattern='(?i)/add', forwards=False, outgoing=False))
+async def ad_handler(event):
+    await client.send_message(event.sender_id, 'Send us an event and we\'ll add it to the list\n')
+
+@client.on(events.NewMessage(pattern='(?i)/about', forwards=False, outgoing=False))
+async def about_handler(event):
+    await client.send_message(event.sender_id, 'About Us\n')
+
+
 
 @events.register(events.NewMessage(incoming=True, outgoing=False))
 async def handler(event):
-#    if '(?i)/events' in event.raw_text:
     if 'Get New Events' in event.raw_text:
         content = get_event_content('new', fulmo_url)
         formatted_text = parse_content(content)
@@ -77,17 +98,30 @@ async def inline_handler(event):
     result = None
     query = event.text.lower()
     if query == '':
+        file_in_bytes=51000
+        thumb_link = 'https://i.imgur.com/VY44NqO.jpg'
+        url_link= 'https://www.ccn.com/wp-content/uploads/2013/12/bitcoin-image.jpg'
+        icon = InputWebDocument(thumb_link, file_in_bytes, 'image/png',[])
+        
         result = builder.article(
             'New Events',
             text=NEW_EVENTS,
-            buttons=custom.Button.url('Join the group!', 't.me/TelethonChat'),
+            thumb= icon,
+            buttons=custom.Button.url('Fulmo: Building the Lightning Network', 'https://fulmo.org/'),
             link_preview=True
-        )
+            )
+        result2 = builder.article(
+            'Past Events',
+            text=PAST_EVENTS,
+            thumb= icon,
+            buttons=custom.Button.url('Fulmo: Building the Lightning Network', 'https://fulmo.org/'),
+            link_preview=True
+            )
 
     # NOTE: You should always answer, but we want plugins to be able to answer
     #       too (and we can only answer once), so we don't always answer here.
     if result:
-        await event.answer([result])
+        await event.answer([result, result2])
 
 
 # ==============================   Inline  ==============================
